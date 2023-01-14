3dcbee0fa0666d421f8be09db029706242f8cd4a
Javokhir <jtursunboev@bk.ru>
2023-01-14 22:23:29 +0900

    js started

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// select elements
+
+const mainContainer = document.querySelector('.container')
+const btnSub = document.querySelector('.btn')
+const rating = document.querySelectorAll('li')
+
+const thankContainer = document.querySelector('#hide__window')
+const rateText = document.querySelector('#change__text')
